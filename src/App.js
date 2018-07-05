bf107ce53f705f2b778945bb2e2c5af9d220310f
Devansh Batra <devanshbatra04@gmail.com>
2018-07-05 15:27:44 +0530

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 import Navbar from './components/layout/navbar'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar/>
-        <h1>Hello</h1>
+        <Router>
+          <div className="App">
+            <Navbar/>
+            <h1>Hello</h1>
 
-      </div>
+          </div>
+        </Router>
     );
   }
 }
