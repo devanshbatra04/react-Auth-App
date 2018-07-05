f99d6dd9a9039cbcb77615e1783fa3a54dddcad9
Devansh Batra <devanshbatra04@gmail.com>
2018-07-05 19:46:37 +0530

    Auth details in data route

--- src/components/pages/Data.js
+++ src/components/pages/Data.js
@@ -1,8 +1,27 @@
-import React from 'react';
-export default () => {
-    return(
-        <div>
-            Data
-        </div>
-    )
+import React, {Component} from 'react';
+
+class MyComponent extends Component {
+    state = {
+        currentUserName: '',
+        currentUserEmail:''
+    };
+    componentDidMount(){
+        const idToken = JSON.parse(localStorage.getItem('okta-token-storage'));
+        this.setState({
+            currentUserName: idToken.idToken.claims.name,
+            currentUserEmail: idToken.idToken.claims.email
+
+        })
+    }
+    render() {
+        console.log(this.state)
+        return (
+            <div>
+
+            </div>
+        );
+    }
 }
+
+
+export default MyComponent;
