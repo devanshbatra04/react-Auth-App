2dc27c76e107346ff2fb40021a3818bcdc6a7390
Devansh Batra <devanshbatra04@gmail.com>
2018-07-05 20:24:23 +0530

    initial get request using reddit api

--- src/components/visualisation/Graph.js
+++ src/components/visualisation/Graph.js
@@ -1,7 +1,23 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class Graph extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: []
+        };
+    }
+
+    componentDidMount(){
+        axios.get(`http://www.reddit.com/r/reactjs.json`)
+            .then(res =>{
+                const posts = res.data.data.children.map(obj => obj.data);
+                this.setState({ posts });
+                console.log(posts);
+            })
+    }
 
     render() {
         return (
