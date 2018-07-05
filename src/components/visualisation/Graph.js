0ffe84c0450edf067a6ab155effa38f932b11ae7
Devansh Batra <devanshbatra04@gmail.com>
2018-07-05 23:20:18 +0530

    SVG DONE YAYYY

--- src/components/visualisation/Graph.js
+++ src/components/visualisation/Graph.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import * as d3 from 'd3';
 
 class Graph extends Component {
 
@@ -18,32 +19,64 @@ class Graph extends Component {
                 // this.setState({ posts });
                 const posts = res.data.dataset.data.map(obj => {
                     return {
-                        date: obj[0],
+                        date: new Date(obj[0]),
                         value: obj[4]
                     }
                 });
                 this.setState({posts});
-                console.log(this.state.posts)
+                console.log(this.state.posts);
                 this.drawChart(this.state.posts);
             })
 
     }
     drawChart(data) {
-        // var svgWidth = 600, svgHeight = 400;
-        // var margin = {top: 20, right: 20, bottom: 30, left: 50};
-        // var width = svgWidth - margin.left - margin.right;
-        // var height = svgHeight - margin.top - margin.bottom;
-        // var svg = d3.select('svg')
-        //     .attr("width", svgWidth)
-        //     .attr("height", svgHeight);
-        console.log(this.state)
+        var svgWidth = 600, svgHeight = 400;
+        var margin = {top: 20, right: 20, bottom: 30, left: 50};
+        var width = svgWidth - margin.left - margin.right;
+        var height = svgHeight - margin.top - margin.bottom;
+        var svg = d3.select('svg')
+            .attr("width", svgWidth)
+            .attr("height", svgHeight);
+        var g = svg.append("g")
+            .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")"
+            );
+        var x = d3.scaleTime().rangeRound([0, width]);
+        var y = d3.scaleLinear().rangeRound([height, 0]);
+        var line = d3.line()
+            .x(function(d) { return x(d.date)})
+            .y(function(d) { return y(d.value)})
+        x.domain(d3.extent(data, function(d) { return d.date }));
+        y.domain(d3.extent(data, function(d) { return d.value }));
+        g.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x))
+            .select(".domain")
+            .remove();
+        g.append("g")
+            .call(d3.axisLeft(y))
+            .append("text")
+            .attr("fill", "#000")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", "0.71em")
+            .attr("text-anchor", "end")
+            .text("Price ($)");
+        g.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-linejoin", "round")
+            .attr("stroke-linecap", "round")
+            .attr("stroke-width", 1.5)
+            .attr("d", line);
+
     }
 
     render() {
         return (
             <div>
-                {this.props.url}
-                <svg></svg>
+                <svg className="line-chart"></svg>
             </div>
         );
     }
